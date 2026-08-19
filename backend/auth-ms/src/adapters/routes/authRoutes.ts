e22923137c3c10d29509273edde64546484b8ed2
unknown <shadsdemon@gmail.com>
2026-08-19 14:35:48 -0500

    endpoint de prueba funcional

--- backend/auth-ms/src/adapters/routes/authRoutes.ts
+++ backend/auth-ms/src/adapters/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router()
 
-router.get("get", (_req, res)=>{
+router.get("/get", (_req, res)=>{
   res.status(200).json({message: "el microservicio esta corriendo"})
 })
 
