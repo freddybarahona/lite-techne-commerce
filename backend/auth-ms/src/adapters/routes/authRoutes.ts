e2e9f1bb9829b11e216fffec6fe995b116f8adff
unknown <shadsdemon@gmail.com>
2026-08-19 14:29:07 -0500

    endpoint de prueba con ruta especifica

---
+++ backend/auth-ms/src/adapters/routes/authRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+const router = Router()
+
+router.get("get", (_req, res)=>{
+  res.status(200).json({message: "el microservicio esta corriendo"})
+})
+
+export default router
