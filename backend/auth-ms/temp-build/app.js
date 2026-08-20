67c90a5dc9015f969295a688c570f1a9c69e954a
unknown <shadsdemon@gmail.com>
2026-08-20 14:07:52 -0500

    borrado archivos temporales

--- backend/auth-ms/temp-build/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import cors from "cors";
-import authRouter from "./src/adapters/routes/authRoutes";
-const app = express();
-app.use(express.json());
-app.use(cors({ origin: "*" }));
-//registro de rutas
-app.use("/auth", authRouter);
-export default app;
