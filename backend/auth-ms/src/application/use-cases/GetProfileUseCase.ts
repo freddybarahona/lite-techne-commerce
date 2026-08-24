2627445ddf8067b91e9d0139153f0638d18ff088
unknown <shadsdemon@gmail.com>
2026-08-24 14:32:16 -0500

    finalizacion authms v0.5

--- backend/auth-ms/src/application/use-cases/GetProfileUseCase.ts
+++ backend/auth-ms/src/application/use-cases/GetProfileUseCase.ts
@@ -1,6 +1,7 @@
 import { formResponse } from "../../adapters/middlewares/FormResponse";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { ResponseConstants } from "../../shared/constants/ResponseConstants";
+import { UserMapper } from "../mappers/user/userMapper";
 import { GetProfileRequest } from "../model/requests/GetProfileRequest";
 import { GenericResponse } from "../responses/GenericResponse";
 
@@ -9,10 +10,11 @@ export class GetProfileUseCase{
   
   async verifica(data: GetProfileRequest): Promise<GenericResponse>{
 
-    const rsp = await this.repository.findByEmail(data.email)
-    console.log(rsp)
-    if(rsp == null)
-      return formResponse(false, 400, [ResponseConstants.USER_FOUND_NOT], rsp)
+    const result = await this.repository.findByEmail(data.email)
+    
+    if(result == null)
+      return formResponse(false, 400, [ResponseConstants.USER_FOUND_NOT], result)
+    const rsp= UserMapper.mapDTO(result)
     
     return formResponse(true, 200, [ResponseConstants.USER_FOUND_YES], rsp)
   }
