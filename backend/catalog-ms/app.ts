363ae07d4e3a6489e97478bc8a6373eca8e31244
unknown <shadsdemon@gmail.com>
2026-08-25 11:37:02 -0500

    generacion de los prefijos de los endpoints

--- backend/catalog-ms/app.ts
+++ backend/catalog-ms/app.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import cors from "cors"
+import catalogRoutes from "./src/adapters/routes/catalogRoutes"
+import productRoutes from "./src/adapters/routes/productRoutes"
 
 const app = express()
 app.use(express.json())
 app.use(cors({origin: "*"}))
 
 //registro de rutas
-
+app.use("/catalog", catalogRoutes)
+app.use("/product", productRoutes)
 
 export default app
