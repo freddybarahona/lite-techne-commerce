7decf415fc8bca884e0b90f4b8088aa47128e96b
Freddy Barahona <shadsdemon@gmail.com>
2026-08-27 13:55:27 -0500

    agregado de comentarios en env.ts

--- backend/inventory-ms/src/infrasctructure/config/env/env.ts
+++ backend/inventory-ms/src/infrasctructure/config/env/env.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv"
 
-dotenv.config()
+dotenv.config() 
+//esto tiene que ejecutarse primero para que las variables 
+//de entorno no se pierdan en el traslado asi que mejor 
+//dejalo asi
 
 export const env ={
   port: Number(process.env.PORT)
