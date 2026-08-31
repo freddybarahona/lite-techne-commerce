37e79599b26f4ed12af4de88d1028969ea73f4b3
Freddy Barahona <shadsdemon@gmail.com>
2026-08-31 13:34:13 -0500

    correccion de errores

---
+++ backend/src/microservices/inventory/src/features/product/inventory.repository.ts
@@ -0,0 +1,37 @@
+import { Repository } from "typeorm";
+import { Inventory } from "../../domain/entities/inventory";
+import { IInventoryRepository } from "./inventory.repository.interface";
+import { AppDataSource } from "../../infrastructure/config/database/data.source";
+import { MyConnectionOptions } from "./inventory.types";
+import { Environment } from "../../infrastructure/config/env/env";
+
+export class InventoryRepository implements IInventoryRepository{
+  constructor(
+    private repository: Repository<Inventory>,
+    private readonly connect: MyConnectionOptions,
+    private readonly env: Environment
+  ){}
+  private async infoRepo(){
+    const dataSource= await AppDataSource.dataSource(this.connect, this.env)
+    this.repository= dataSource.getRepository(Inventory)
+  }
+
+  createInventory(data: Inventory): Promise<Inventory> {
+    throw new Error("Method not implemented.");
+  }
+  ifExistsInventoryByName(data: string): Promise<Boolean> {
+    throw new Error("Method not implemented.");
+  }
+  getAllInventory(): Promise<Inventory[]> {
+    throw new Error("Method not implemented.");
+  }
+  getInventoryById(data: number): Promise<Inventory | null> {
+    throw new Error("Method not implemented.");
+  }
+  modInventoryById(data: Inventory): Promise<Inventory> {
+    throw new Error("Method not implemented.");
+  }
+  deleteInventory(data: number): Promise<Boolean> {
+    throw new Error("Method not implemented.");
+  }
+}
