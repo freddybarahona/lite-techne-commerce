3a0b9f7ae690b7f9286402840328081bfc551c38
Freddy Barahona <shadsdemon@gmail.com>
2026-09-01 03:30:02 -0500

    arreglos menores

--- backend/legacy/inventory-ms/src/server.ts
+++ backend/legacy/inventory-ms/src/server.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata"
+import "reflect-metadata" 
 import app from "./app"
 import { initializeConstants } from "./shared/constants/InitializeConstants"
 import { env } from "./infrasctructure/config/env/env"
