bc37554d93cbd2b4d163ac7cae6183d7d0149fbd
Freddy Barahona <shadsdemon@gmail.com>
2026-09-01 14:16:03 -0500

    arreglo de rutas no ingresa a las rutas correctamente se cuelga antes de entrar

--- backend/src/microservices/inventory/src/core/config/config.ts
+++ backend/src/microservices/inventory/src/core/config/config.ts
@@ -1,4 +1,3 @@
-import { DataSource } from "typeorm";
 import { Environment } from "../../infrastructure/config/env/env";
 import {InitializeConstants} from "../../../../shared/constants/initialize.constants"
 import AppDataSource from "../../infrastructure/config/database/data.source";
@@ -8,7 +7,7 @@ export default class serverconfigurations{
     env = new Environment
     dbSource= new AppDataSource().dataSource
     back= new AppCore
-    
+
     initializeDBandBack(){
       this.dbSource.initialize().then(()=>{
           console.log(InitializeConstants.dbConnectionEstablished({db:this.env.db_name, port:this.env.db_port}))
