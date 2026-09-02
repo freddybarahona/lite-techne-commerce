dc37cec74895eca1cd0b05234432ea72cbc0e7bb
Freddy Barahona <shadsdemon@gmail.com>
2026-09-02 08:38:05 -0500

    arreglo del express.json() no lo olvides SIIIIII

--- backend/src/microservices/inventory/src/core/app.core.ts
+++ backend/src/microservices/inventory/src/core/app.core.ts
@@ -10,11 +10,16 @@ export default class AppCore{
     this.config()
   }
   private config(){
-    this.app.use(express.json)
+    this.app.use(express.json()) 
+    //express.json MAL | express.json() BIEN
+    /* que te quede de experiencia este detalle
+    no te lo marca el nodejs pero te lo marcara el postman quedandose
+    colgado con cada request asi que necesitas figarte en esos detalles */
+    
     console.log("ingreso a la configuracion")
     
     this.app.use(cors({origin: "*"}))
-    const env= new Environment
+    const env= new Environment()
     const makers = new InventoryMakers(env)
     const inventoryRoutes= new InventoryRoutes(makers)
     //informacion de rutas de rutas
