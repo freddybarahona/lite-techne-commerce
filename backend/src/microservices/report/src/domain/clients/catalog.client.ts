38bf796bb2fa383e0c2c1b4874f411d25f479e8d
Freddy Barahona <shadsdemon@gmail.com>
2026-09-07 10:28:36 -0500

    correccion menor

--- backend/src/microservices/report/src/domain/clients/catalog.client.ts
+++ backend/src/microservices/report/src/domain/clients/catalog.client.ts
@@ -1,4 +1,35 @@
 
 export class CatalogClient{
+  private products = new Map<number, {
+    product_id: number
+    name: number
+    price: number
+    category: string
+  }>()
 
+  handleEvent(event: string, data: any): void{
+    switch (event){
+      case "catalog.product.created":
+      case "catalog.product.updated": 
+        this.products.set(Number(data.product_id), {
+          product_id: Number(data.product_id),
+          name: Number(data.stock),
+          price: Number(data.price),
+          category: String(data.category)
+        })
+        break
+      case "catalog.product.deleted":
+        this.products.delete(Number(data.product_id))
+        break
+    }
+
+  }
+
+  getAll(){
+    return [...this.products.values()]
+  }
+
+  getByProductId(id: number){
+    return this.products.get(id)
+  }
 }
