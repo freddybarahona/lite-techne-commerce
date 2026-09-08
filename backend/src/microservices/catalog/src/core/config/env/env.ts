16d9c3e82a847c0e8ed594a09b352fbfcb0c5505
Freddy Barahona <shadsdemon@gmail.com>
2026-09-08 14:57:01 -0500

    ya falta poco dios

--- backend/src/microservices/catalog/src/core/config/env/env.ts
+++ backend/src/microservices/catalog/src/core/config/env/env.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export class Environment implements Ienv{
   port= Number(process.env.PORT_BACK)
-  db_host= String(process.env.DB_HOST)
+  db_host= Boolean(process.env.DOCKER)? String(process.env.DB_HOST_DOCKER) : String(process.env.DB_HOST)
   db_port= Number(process.env.DB_PORT)
   db_user= String(process.env.DB_USER)
   db_password= String(process.env.DB_PASSWORD)
