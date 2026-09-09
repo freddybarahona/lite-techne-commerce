5db6b2253dcd3d42ed9b680c541ab4b7e51905db
Freddy Barahona <shadsdemon@gmail.com>
2026-09-09 11:29:18 -0500

    arreglos de inicializacion de rabbit mq completados e imagen docker generada con exito

--- backend/src/microservices/shared/env/env.ts
+++ backend/src/microservices/shared/env/env.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv"
 dotenv.config()
 export class env {
   //info de rabbitmq
-  rabbitmq_url=String(process.env.RABBITMQ_URL)
+  rabbitmq_url=Boolean(process.env.DOCKER)? String(process.env.RABBITMQ_URL_DOCKER) : String(process.env.RABBITMQ_URL)
 }
