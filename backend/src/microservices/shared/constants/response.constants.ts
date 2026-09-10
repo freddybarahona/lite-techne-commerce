906dd0ad6f190cdf014d01fd7f465bfb7111e34c
Freddy Barahona <shadsdemon@gmail.com>
2026-09-10 14:58:36 -0500

    arreglos del env y de responseConstants

--- backend/src/microservices/shared/constants/response.constants.ts
+++ backend/src/microservices/shared/constants/response.constants.ts
@@ -42,4 +42,5 @@ export class ResponseConstants{
 
   static CATEGORY_ALREADY_EXISTS= "esta categoria ya existe"
   static ERASED_ELEMENT= "elemento borrado correctamente" 
+  static PRODUCT_ALREADY_EXISTS= "este producto ya existe"
 }
