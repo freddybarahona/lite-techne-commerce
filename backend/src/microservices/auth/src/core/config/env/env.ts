30a1816622425ca7b837ef1026e57fd2c19eda6b
Freddy Barahona <shadsdemon@gmail.com>
2026-09-10 12:18:49 -0500

    modificacion del datasource con flags en .env para mejor dockerizado

--- backend/src/microservices/auth/src/core/config/env/env.ts
+++ backend/src/microservices/auth/src/core/config/env/env.ts
@@ -11,8 +11,8 @@ export class Environment implements Ienv{
   db_user= String(process.env.DB_USER)
   db_password= String(process.env.DB_PASSWORD)
   db_name= String(process.env.DB_NAME)
-  synchronize= Boolean(process.env.SYNCHRONIZE)
-  logging= Boolean(process.env.SYNCHRONIZE) ? true : false
+  synchronize= process.env.SYNCHRONIZE === "true" ? true : false
+  logging= process.env.LOGGING === "true" ? true : false
   jwt_secret= String(process.env.JWT_SECRET)
 
   role_1_id= Number(process.env.ROLE_1_ID)
