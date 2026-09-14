eb106c81bfd75bb1c3ca9e61539b847769b80f40
Freddy Barahona <shadsdemon@gmail.com>
2026-09-14 11:35:57 -0500

    arreglos de develop

--- backend/src/microservices/shared/env/env.ts
+++ backend/src/microservices/shared/env/env.ts
@@ -1,28 +1,7 @@
 import dotenv from "dotenv"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 dotenv.config()
-export class env {
-  //info de rabbitmq
-  rabbitmq_url=String(process.env.RABBITMQ_URL)
-
-
-}
-=======
-=======
->>>>>>> feature/inventory-ms
-=======
->>>>>>> feature/report-ms
-
 export class env {
   //info de rabbitmq
   rabbitmq_url=Boolean(process.env.DOCKER)? String(process.env.RABBITMQ_URL_DOCKER) : String(process.env.RABBITMQ_URL)
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> feature/catalog-ms
-=======
->>>>>>> feature/inventory-ms
-=======
->>>>>>> feature/report-ms
+
