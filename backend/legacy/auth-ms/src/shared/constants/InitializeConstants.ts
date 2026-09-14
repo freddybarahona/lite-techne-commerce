e409e4ed937db41a77a7266cb72d4f87207c46f7
Freddy Barahona <shadsdemon@gmail.com>
2026-09-14 13:46:49 -0500

    arreglos de auth

--- backend/legacy/auth-ms/src/shared/constants/InitializeConstants.ts
+++
@@ -1,6 +0,0 @@
-export class InitializeConstants{
-  static DB_CONNECTION_ESTABLISHED="Conexion BD establecida"
-  static infoBackActive(port: number){
-    return `Auth service corriendo en http://localhost:${port}`
-  }
-}
