6044bc9639bc07f1f6bc166a249e43fdf5380453
Freddy Barahona <shadsdemon@gmail.com>
2026-09-16 11:49:06 -0500

    arreglos de diseno con tipos

--- frontend/src/app/shared/services/auth.service.ts
+++ frontend/src/app/shared/services/auth.service.ts
@@ -1,26 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment'; 
+//los imports de environment que sean a produccion porque angular lo resolvera por su cuenta si no funciona
 import { Observable } from 'rxjs';
 import { GenericResponse } from '../../core/interfaces/generic.response';
 import { UserDTO } from '../interfaces/user.dto.interface';
 import { LoginRequest } from '../interfaces/login.request.interface';
 import { RegisterRequest } from '../interfaces/register.request.interface';
+import { ramas_disp } from '../../ramas';
 
 @Service()
 export class Auth {
   private http= inject(HttpClient)
-  private branch = `${environment.back_url}/auth`
+  private url = `${environment.back_url}`
+  private rama : ramas_disp= "/auth/"
 
   getProfile(): Observable<GenericResponse<UserDTO>>{
-    return this.http.get<GenericResponse<UserDTO>>(`${this.branch}/auth/profile`)
+    return this.http.get<GenericResponse<UserDTO>>(`${this.url}${this.rama}profile`)
   }
 
   login(request: LoginRequest): Observable<GenericResponse<string>>{
-    return this.http.post<GenericResponse<string>>(`${this.branch}/auth/login`, request)
+    return this.http.post<GenericResponse<string>>(`${this.url}${this.rama}login`, request)
   }
 
   registerUser(request: RegisterRequest): Observable<GenericResponse<UserDTO>>{
-    return this.http.post<GenericResponse<UserDTO>>(`${this.branch}/auth/register`, request)
+    return this.http.post<GenericResponse<UserDTO>>(`${this.url}${this.rama}register`, request)
   }
 }
