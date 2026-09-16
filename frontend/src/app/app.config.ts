6d3775a6f2f887bb4e50234b03337a06dae133c2
Freddy Barahona <shadsdemon@gmail.com>
2026-09-16 14:48:06 -0500

    arreglo de animaciones para el footer en proceso

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,12 +5,14 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations()
   ]
 };
