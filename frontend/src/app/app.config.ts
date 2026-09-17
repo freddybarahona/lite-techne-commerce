4d762bcc2295d513a8546d19b839f1ba035e4f75
Freddy Barahona <shadsdemon@gmail.com>
2026-09-17 08:25:42 -0500

    arreglos para animaciones en footer completado

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -13,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimations()
   ]
 };
 
