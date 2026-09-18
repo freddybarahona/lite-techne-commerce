5b1aacdf9bb26d261f9a2c0b16ee286e6568a13b
Freddy Barahona <shadsdemon@gmail.com>
2026-09-18 10:13:51 -0500

    anadido de cart para frontend en proceso

---
+++ frontend/src/app/features/customer/cart.component/cart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cart.component',
+  styleUrl: './cart.component.css',
+  templateUrl: './cart.component.html',
+})
+export class CartComponent {}
