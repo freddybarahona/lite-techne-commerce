23f63ce10408dd58671fdf35dca094df3ca339b6
Freddy Barahona <shadsdemon@gmail.com>
2026-09-18 13:59:33 -0500

    arreglos del cart

--- frontend/src/app/features/customer/cart.component/cart.component.ts
+++ frontend/src/app/features/customer/cart.component/cart.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-cart.component',
-  styleUrl: './cart.component.css',
   templateUrl: './cart.component.html',
 })
 export class CartComponent {}
